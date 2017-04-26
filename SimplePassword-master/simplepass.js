9c1cb1d8275c615c3161a5e5b820ee8533ffc7c7
rteliz <9rteliz@stpatrick.org>
2017-04-26 14:00:00 -0500

    2/26/17

---
+++ SimplePassword-master/simplepass.js
@@ -0,0 +1,12 @@
+//type checkString below
+function checkString(str){
+  if(str.length<8 && str.length>20){
+    throw RangeError("Password needs to be between 8-20");
+  }
+  else if{
+    return true
+  }
+}
+
+
+//type getString below
